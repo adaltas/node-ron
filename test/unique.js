00c7c35619d8e4211a520e996252a6c477560d2c
David Worms <david@adaltas.com>
2011-03-04 18:48:27 +0100

    Add configure method, improve unique deletion

--- test/unique.js
+++ test/unique.js
@@ -152,100 +152,107 @@ module.exports = {
 			});
 		});
 	},
-	// 'Test clear': function(callback){
-	// 	var model = ron.create('TestUniqueClear').unique('property');
-	// 	model.put({
-	// 		property: 'my value'
-	// 	},function(err,record){
-	// 		assert.ifError(err);
-	// 		model.clear(function(err){
-	// 			assert.ifError(err);
-	// 			var multi = ron.client().multi();
-	// 			multi.type('count:TestUniqueClear',function(err,type){
-	// 				assert.eql('none',type);
-	// 			});
-	// 			multi.type('obj:TestUniqueClear:'+record.id,function(err,type){
-	// 				assert.eql('none',type);
-	// 			});
-	// 			multi.type('index:TestUniqueClear',function(err,type){
-	// 				assert.eql('none',type);
-	// 			});
-	// 			multi.type('index:TestUniqueClear:property',function(err,type){
-	// 				assert.eql('none',type);
-	// 			});
-	// 			multi.exec(function(err,results){
-	// 				// ron.quit();
-	// 				callback();
-	// 			});
-	// 		});
-	// 	});
-	// },
-	// 'Test get': function(callback){
-	// 	var model = ron.create('TestUniqueGet').unique('property');
-	// 	model.put({
-	// 		property: 'my value'
-	// 	},function(err,record){
-	// 		model.get('property', 'my value', function(err,record){
-	// 			assert.ifError(err);
-	// 			assert.ok( !isNaN(parseInt(record.id)) );
-	// 			assert.eql('my value',record.property);
-	// 			model.clear(function(err){
-	// 				assert.ifError(err);
-	// 				// ron.quit();
-	// 				callback();
-	// 			});
-	// 		});
-	// 	});
-	// },
-	// 'Test delete': function(callback){
-	// 	var model = ron.create('TestUniqueDelete').unique('property_1').unique('property_2');
-	// 	model.put({
-	// 		property_1: 'my value 1',
-	// 		property_2: 'my value 2'
-	// 	},function(err,record){
-	// 		assert.ifError(err);
-	// 		model.delete(record.id,function(err,id){
-	// 			assert.ifError(err);
-	// 			var multi = ron.client().multi();
-	// 			multi.type('count:TestUniqueDelete',function(err,type){
-	// 				assert.eql('string',type);
-	// 			});
-	// 			multi.type('obj:TestUniqueDelete:'+id,function(err,type){
-	// 				assert.eql('none',type);
-	// 			});
-	// 			multi.scard('index:TestUniqueDelete',function(err,length){
-	// 				assert.eql(0,length);
-	// 			});
-	// 			multi.hgetall('index:TestUniqueDelete:property',function(err,value){
-	// 				assert.eql({},value);
-	// 			});
-	// 			multi.exec(function(err,results){
-	// 				model.clear(function(err){
-	// 					callback();
-	// 				});
-	// 			});
-	// 		});
-	// 	});
-	// },
-	// 'Test unique query': function(callback){
-	// 	var model = ron.create('TestUniqueQuery').unique('property');
-	// 	model.put([{
-	// 		property: 'value 1'
-	// 	},{
-	// 		property: 'value 2'
-	// 	},{
-	// 		property: 'value 3'
-	// 	}],function(err,records){
-	// 		model.get({property:['value 1','value 3']},function(err,records){
-	// 			assert.eql(2,records.length);
-	// 			assert.eql('value 1',records[0].property);
-	// 			assert.eql('value 3',records[1].property);
-	// 			model.clear(function(err){
-	// 				callback();
-	// 			});
-	// 		})
-	// 	});
-	// },
+	'Test clear': function(callback){
+		var model = ron.create('TestUniqueClear').unique('property');
+		model.put({
+			property: 'my value'
+		},function(err,record){
+			assert.ifError(err);
+			model.clear(function(err){
+				assert.ifError(err);
+				var multi = ron.client().multi();
+				multi.type('count:TestUniqueClear',function(err,type){
+					assert.eql('none',type);
+				});
+				multi.type('obj:TestUniqueClear:'+record.id,function(err,type){
+					assert.eql('none',type);
+				});
+				multi.type('index:TestUniqueClear',function(err,type){
+					assert.eql('none',type);
+				});
+				multi.type('index:TestUniqueClear:property',function(err,type){
+					assert.eql('none',type);
+				});
+				multi.exec(function(err,results){
+					// ron.quit();
+					callback();
+				});
+			});
+		});
+	},
+	'Test get': function(callback){
+		var model = ron.create('TestUniqueGet').unique('property');
+		model.put({
+			property: 'my value'
+		},function(err,record){
+			model.get('property', 'my value', function(err,record){
+				assert.ifError(err);
+				assert.ok( !isNaN(parseInt(record.id)) );
+				assert.eql('my value',record.property);
+				model.clear(function(err){
+					assert.ifError(err);
+					callback();
+				});
+			});
+		});
+	},
+	'Test get missing': function(callback){
+		var model = ron.create('TestUniqueMissing').unique('property');
+		model.get('property', 'missing value', function(err,record){
+			assert.ifError(err);
+			assert.eql(null,record);
+			callback();
+		});
+	},
+	'Test delete': function(callback){
+		var model = ron.create('TestUniqueDelete').unique('property_1').unique('property_2');
+		model.put({
+			property_1: 'my value 1',
+			property_2: 'my value 2'
+		},function(err,record){
+			assert.ifError(err);
+			model.delete(record.id,function(err,id){
+				assert.ifError(err);
+				var multi = ron.client().multi();
+				multi.type('count:TestUniqueDelete',function(err,type){
+					assert.eql('string',type);
+				});
+				multi.type('obj:TestUniqueDelete:'+id,function(err,type){
+					assert.eql('none',type);
+				});
+				multi.scard('index:TestUniqueDelete',function(err,length){
+					assert.eql(0,length);
+				});
+				multi.hgetall('index:TestUniqueDelete:property',function(err,value){
+					assert.eql({},value);
+				});
+				multi.exec(function(err,results){
+					model.clear(function(err){
+						callback();
+					});
+				});
+			});
+		});
+	},
+	'Test unique query': function(callback){
+		var model = ron.create('TestUniqueQuery').unique('property');
+		model.put([{
+			property: 'value 1'
+		},{
+			property: 'value 2'
+		},{
+			property: 'value 3'
+		}],function(err,records){
+			model.get({property:['value 1','value 3']},function(err,records){
+				assert.eql(2,records.length);
+				assert.eql('value 1',records[0].property);
+				assert.eql('value 3',records[1].property);
+				model.clear(function(err){
+					callback();
+				});
+			})
+		});
+	},
 	'quit': function(callback){
 		ron.quit();
 		callback();
